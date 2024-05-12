63010e6e3134d95ed1ba0d42359c0766300b5d01
lewisndungu <lewis@jahazii.io>
2024-05-12 15:07:06 +0300

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterLink, RouterOutlet } from "@angular/router";
+import { NzIconModule } from "ng-zorro-antd/icon";
+import { NzLayoutModule } from "ng-zorro-antd/layout";
+import { NzMenuModule } from "ng-zorro-antd/menu";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [CommonModule, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule, RouterLink],
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'zeraki-agent-dashboard';
+  isCollapsed = false;
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", pathMatch: "full", redirectTo: "/dashboard" },
+  { path: "welcome", loadChildren: () => import("./pages/welcome/welcome.routes").then(m => m.WELCOME_ROUTES) },
+  { path: "dashboard", loadChildren: () => import("./pages/dashboard/dashboard.routes").then(m => m.DASHBOARD_ROUTES) },
+  {
+    path: "school-management",
+    loadChildren: () => import("./pages/school-management/school-management.routes").then(m => m.SCHOOL_MANAGEMENT_ROUTES)
+  }
+];
