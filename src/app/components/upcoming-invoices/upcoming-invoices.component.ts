f6bdd7e38a9251677e70c98dcaa70ae9cee0494f
lewisndungu <lewis@jahazii.io>
2024-05-14 08:15:36 +0300

    Update and delete upcoming invoices

--- src/app/components/upcoming-invoices/upcoming-invoices.component.ts
+++ src/app/components/upcoming-invoices/upcoming-invoices.component.ts
@@ -5,13 +5,17 @@ import { NzButtonComponent } from "ng-zorro-antd/button";
 import { NzBadgeComponent } from "ng-zorro-antd/badge";
 import { NzTagComponent } from "ng-zorro-antd/tag";
 import { DatePipe } from "../../pipes/date.pipe";
-import { NzModalModule } from "ng-zorro-antd/modal";
+import { NzModalModule, NzModalService } from "ng-zorro-antd/modal";
 import { NzFormModule } from "ng-zorro-antd/form";
 import { InvoicesService } from "../../services/invoices.service";
 import { Invoice } from "../../types/types";
 import { CollectPaymentComponent } from "../collect-payment/collect-payment.component";
 import { Subscription } from "rxjs";
 import { InvoiceListComponent } from "../invoice-list/invoice-list.component";
+import { SchoolsService } from "../../services/schools.service";
+import { ActivatedRoute } from "@angular/router";
+import { NzMessageService } from "ng-zorro-antd/message";
+import { CreateInvoiceComponent } from "../create-invoice/create-invoice.component";
 
 @Component({
   selector: "app-upcoming-invoices",
@@ -29,6 +33,7 @@ import { InvoiceListComponent } from "../invoice-list/invoice-list.component";
     NgIf,
     CollectPaymentComponent,
     InvoiceListComponent,
+    CreateInvoiceComponent,
   ],
   templateUrl: "./upcoming-invoices.component.html",
   styleUrl: "./upcoming-invoices.component.css",
@@ -49,7 +54,21 @@ export class UpcomingInvoicesComponent implements OnInit, OnDestroy {
     isAddingCollectionToInvoice: false,
   };
 
-  constructor(readonly invoiceService: InvoicesService) {}
+  invoiceUpdateCreateState: {
+    status: "idle" | "updating" | "creating";
+    selectedSchool?: { name: string; id: number };
+    selectedInvoice?: Invoice;
+  } = {
+    status: "idle",
+  };
+
+  constructor(
+    readonly invoiceService: InvoicesService,
+    readonly schoolService: SchoolsService,
+    readonly route: ActivatedRoute,
+    readonly modal: NzModalService,
+    readonly message: NzMessageService,
+  ) {}
 
   handleCollection(invoice: Invoice) {
     this.invoicePaymentCollectionState = {
@@ -74,6 +93,54 @@ export class UpcomingInvoicesComponent implements OnInit, OnDestroy {
 
   closeCollectionModal() {
     this.invoicePaymentCollectionState = { isAddingCollectionToInvoice: false };
+  }
+
+  handleUpdateInvoice(invoice: Invoice) {
+    this.invoiceUpdateCreateState = {
+      status: "updating",
+      selectedInvoice: invoice,
+      selectedSchool: {
+        name: "Githinji Secondary School",
+        id: 1,
+      },
+    };
+  }
+
+  closeInvoiceCreationModal() {
+    this.invoiceUpdateCreateState = {
+      status: "idle",
+    };
+  }
+
+  handleDeleteInvoice(invoice: Invoice) {
+    this.modal.confirm({
+      nzTitle: "Are you sure delete this invoice?",
+      nzContent: `<p class="text-gray-600""> This action will delete the invoice and is irreversible. Deleting this invoice will remove it from the system. </p>`,
+      nzOkText: "Delete Invoice",
+      nzOkType: "primary",
+      nzOkDanger: true,
+      nzOnOk: () => {
+        let ref = this.message.loading("Deleting invoice...", {
+          nzDuration: 0,
+        }).messageId;
+
+        this.invoiceService.deleteInvoice(invoice.id).subscribe({
+          next: () => {
+            this.message.remove(ref);
+            this.message.success("Invoice deleted successfully");
+            this.fetchUpcomingInvoices();
+          },
+          error: () => {
+            this.modal.error({
+              nzTitle: "An error occurred",
+              nzContent:
+                "An error occurred while deleting the invoice. Please try again.",
+            });
+          },
+        });
+      },
+      nzCancelText: "Cancel",
+    });
   }
 
   ngOnDestroy() {
